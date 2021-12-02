935ce6aa34e989eb5720fc4606ba8d6cca236acd
snegib <aaravbisht20022016@gmail.com>
2021-12-02 21:52:32 +0530

    action creator for fetch user

---
+++ src/client/actions/index.js
@@ -0,0 +1,11 @@
+import Axios from "axios"; // for api request
+
+export const FETCH_USERS = "fetch-users";
+export const fetchUsers = () => async (dispatch) => {
+    const res = await axios.get("https://react-ssr-api.herokuapp.com/users");
+
+    dispatch({
+        type: FETCH_USERS,
+        payload: res, // sending data whatever getting from api
+    });
+};
